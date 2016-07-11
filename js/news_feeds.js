c70210c4470585ec81a0c0ca9b6e3282c73eaca3
livinglegends-j <jlajesse@gmail.com>
2016-07-10 17:04:58 -0700

    rss feed images fixed

--- js/news_feeds.js
+++ js/news_feeds.js
@@ -8,16 +8,17 @@ $(function() {
       function(response) {
         // console.log();
         $.each(response.query.results.item, function (i,newsItem) {
-            $("#AllAccessNews-cont").append('<div class="media"><a class="pull-left" href="#"><img class="media-object" src="img/ALLACCESSnewsimg.png"  alt="100x100" style="width: 100px; height: 100px;"></a><div class="media-body"><h4 class="media-heading">' + newsItem.title + '</h4><p>' + newsItem.description + '</p></div></div>');
+            $("#AllAccessNews-cont").append('<div class="media"><a class="pull-left" href="'+newsItem.link+'"><img class="media-object" src="img/ALLACCESSnewsimg.png"  alt="100x100" style="width: 100px; height: 100px;"></a><div class="media-body"><h4 class="media-heading">' + newsItem.title + '</h4><p>' + newsItem.description + '</p></div></div>');
             return ( i !== (numArticles -1) );
         });
       });
 
       $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Ffeeds.feedburner.com%2FRadiofacts%3Fformat%3Dxml%22&format=json&diagnostics=true&callback=",
         function(response) {
-          // console.log();
           $.each(response.query.results.item, function (i,newsItem) {
-              $("#RadioFacts-cont").append('<div class="media"><a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/100x100/444/fff&text=image"  alt="100x100" style="width: 100px; height: 100px;"></a><div class="media-body"><h4 class="media-heading">' + newsItem.title + '</h4><p>' + newsItem.description + '</p></div></div>');
+              var RF_description = newsItem.description.split("...<br/>")[0];
+              $("#RadioFacts-cont").append('<div class="media"><a class="pull-left" target="_blank" href="'+newsItem.link+'"><img class="media-object" src="img/radiofacts.jpg"  alt="100x100" style="width: 100px; height: 100px;"></a><div class="media-body"><h4 class="media-heading">' + newsItem.title + '</h4><p>' + RF_description + '...<br><br><a target="_blank" href="' + newsItem.link + '">Read more</a></p></div></div>');
+
               return ( i !== (numArticles -1) );
           });
         });
