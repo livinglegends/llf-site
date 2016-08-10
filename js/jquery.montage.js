4fba889506ffe9974c2c4c0af0cb490b99a7da7e
livinglegends <androbaut@Andersons-MacBook-Pro.local>
2016-08-10 14:32:41 -0700

    got rid of crappy photos

--- js/jquery.montage.js
+++ js/jquery.montage.js
@@ -9,13 +9,13 @@
  * Date: Tue Aug 30 2011
  */
 (function( window, $, undefined ) {
-	
+
 	/*
-	* Array.max, Array.min 
+	* Array.max, Array.min
 	* @John Resig
 	* http://ejohn.org/blog/fast-javascript-maxmin/
 	*/
-	
+
 	// function to get the Max value in array
     Array.max 					= function( array ){
         return Math.max.apply( Math, array );
@@ -25,7 +25,7 @@
     Array.min 					= function( array ){
        return Math.min.apply( Math, array );
     };
-	
+
 	/*
 	* smartresize: debounced resize event for jQuery
 	*
@@ -63,7 +63,7 @@
 	$.fn.smartresize 			= function( fn ) {
 		return fn ? this.bind( "smartresize", fn ) : this.trigger( "smartresize", ["execAsap"] );
 	};
-	
+
 	// ======================= imagesLoaded Plugin ===============================
 	// https://github.com/desandro/imagesloaded
 
@@ -110,18 +110,18 @@
 		});
 
 		return this;
-	};	
-	
+	};
+
 	$.Montage 					= function( options, element ) {
 		this.element 	= $( element ).show();
 		this.cache		= {};
 		this.heights	= new Array();
 		this._create( options );
 	};
-	
+
 	$.Montage.defaults 			= {
 		liquid					: true, // if you use percentages (or no width at all) for the container's width, then set this to true
-										// this will set the body's overflow-y to scroll ( fix for the scrollbar's width problem ) 
+										// this will set the body's overflow-y to scroll ( fix for the scrollbar's width problem )
 		margin					: 1,	// space between images.
 		minw					: 70,	// the minimum width that a picture should have.
 		minh					: 20,	// the minimum height that a picture should have.
@@ -135,7 +135,7 @@
 		minsize					: false,// minw,minh are irrelevant. Chosen height is the minimum one available.
 		fillLastRow				: false	// if true, there will be no gaps in the container. The last image will fill any white space available
     };
-	
+
 	$.Montage.prototype 		= {
 		_getImageWidth		: function( $img, h ) {
 			var i_w	= $img.width(),	i_h	= $img.height(), r_i = i_h / i_w;
@@ -151,18 +151,18 @@
 				return Array.min( this.heights );
 			}
 			// otherwise get the most repeated heights. From those choose the minimum.
-			var result 	= {}, 
-				max 	= 0, 
+			var result 	= {},
+				max 	= 0,
 				res, val, min;
-				
+
 			for( var i = 0, total = this.heights.length; i < total; ++i ) {
 				var val	= this.heights[i], inc = ( result[val] || 0 ) + 1;
-				
+
 				if( val < this.options.minh || val > this.options.maxh ) continue;
-				
+
 				result[val] = inc;
-				
-				if( inc >= max ) { 
+
+				if( inc >= max ) {
 					max = inc; res = val;
 				}
 			}
@@ -170,19 +170,19 @@
 				if (result[i] === max) {
 					val = i;
 					min = min || val;
-					
+
 					if(min < this.options.minh)
 						min = null;
 					else if (min > val)
 						min = val;
-					if(min === null) 
+					if(min === null)
 						min = val;
 				}
 			}
 			if(min === undefined) min = this.heights[0];
-			
+
 			res = min;
-			
+
 			return res;
 		},
 		_stretchImage		: function( $img ) {
@@ -192,27 +192,27 @@
 					x 	: new_w,
 					y	: this.theHeight
 				};
-			
+
 			var new_image_w		= $img.width() + this.cache.space_w_left,
 				new_image_h		= this._getImageHeight( $img, new_image_w );
-			
+
 			this._cropImage( $img, new_image_w, new_image_h, crop );
 			this.cache.space_w_left = this.cache.container_w;
 			// if this.options.alternateHeight is true, change row / change height
-			if( this.options.alternateHeight)	
-				this.theHeight			= Math.floor( Math.random()*( this.options.alternateHeightRange.max - this.options.alternateHeightRange.min + 1 ) + this.options.alternateHeightRange.min );		
+			if( this.options.alternateHeight)
+				this.theHeight			= Math.floor( Math.random()*( this.options.alternateHeightRange.max - this.options.alternateHeightRange.min + 1 ) + this.options.alternateHeightRange.min );
 		},
 		_updatePrevImage	: function( $nextimg ) {
 			var $prevImage 		= this.element.find('img.montage:last');
-			
+
 			this._stretchImage( $prevImage );
-			
+
 			this._insertImage( $nextimg );
 		},
 		_insertImage		: function( $img ) {
 			// width the image should have with height = this.theHeight.
 			var new_w = this._getImageWidth( $img, this.theHeight );
-			
+
 			// use the minimum height available if this.options.minsize = true.
 			if( this.options.minsize && !this.options.alternateHeight ) {
 				if( this.cache.space_w_left <= this.options.margin * 2 ) {
@@ -224,14 +224,14 @@
 						this._cropImage( $img, new_w, this.theHeight, crop );
 						this.cache.space_w_left = this.cache.container_w;
 						$img.addClass('montage');
-					}	
+					}
 					else {
 						var crop = { x 	: new_w, y : this.theHeight };
 						this._cropImage( $img, new_w, this.theHeight, crop );
 						this.cache.space_w_left -= new_w;
 						$img.addClass('montage');
 					}
-				}	
+				}
 			}
 			else {
 				// the width is lower than the minimum width allowed.
@@ -240,7 +240,7 @@
 					// need to resize the previous (last) item in that row.
 					if( this.options.minw > this.cache.space_w_left ) {
 						this._updatePrevImage( $img );
-					} 
+					}
 					else {
 						var new_w = this.options.minw, new_h = this._getImageHeight( $img, new_w ), crop = { x : new_w, y : this.theHeight };
 						this._cropImage( $img, new_w, new_h, crop );
@@ -253,7 +253,7 @@
 					// need to resize the previous (last) item in that row.
 					if( new_w > this.cache.space_w_left && this.cache.space_w_left < this.options.minw ) {
 						this._updatePrevImage( $img );
-					}	
+					}
 					else if( new_w > this.cache.space_w_left && this.cache.space_w_left >= this.options.minw ) {
 						var crop = {x : this.cache.space_w_left, y : this.theHeight};
 						this._cropImage( $img, new_w, this.theHeight, crop );
@@ -262,31 +262,31 @@
 						if( this.options.alternateHeight)
 							this.theHeight	= Math.floor( Math.random()*( this.options.alternateHeightRange.max - this.options.alternateHeightRange.min + 1 ) + this.options.alternateHeightRange.min );
 						$img.addClass('montage');
-					}	
+					}
 					else {
 						var crop = { x : new_w, y : this.theHeight};
 						this._cropImage( $img, new_w, this.theHeight, crop );
 						this.cache.space_w_left -= new_w;
 						$img.addClass('montage');
-					}	
+					}
 				}
 			}
 		},
 		_cropImage			: function( $img, w, h, cropParam ) {
 			// margin value
 			var dec = this.options.margin * 2;
-			
+
 			var $wrapper	= $img.parent('a');
-			
+
 			// resize the image
 			this._resizeImage( $img, w, h );
-			
+
 			// adjust the top / left values to slice the image without loosing the its ratio
 			$img.css({
 				left	: - ( w - cropParam.x ) / 2 + 'px',
 				top		: - ( h - cropParam.y ) / 2 + 'px'
-			});	
-			
+			});
+
 			// wrap the image in a <a> element
 			$wrapper.addClass('am-wrapper').css({
 				width	: cropParam.x - dec + 'px',
@@ -300,7 +300,7 @@
 		_reload				: function() {
 			// container's width
 			var new_el_w = this.element.width();
-			
+
 			// if different, something changed...
 			if( new_el_w !== this.cache.container_w ) {
 				this.element.hide();
@@ -312,28 +312,28 @@
 				});
 				if( instance.options.fillLastRow && instance.cache.space_w_left !== instance.cache.container_w ) {
 					instance._stretchImage( instance.$imgs.eq( instance.totalImages - 1 ) );
-				}	
+				}
 				instance.element.show();
 			}
 		},
 		_create 			: function( options ) {
 			this.options 	= $.extend( true, {}, $.Montage.defaults, options );
-			
+
 			var instance 		= this,
 				el_w 			= instance.element.width();
-			
+
 			instance.$imgs		= instance.element.find('img');
 			instance.totalImages= instance.$imgs.length;
-			
+
 			// solve the scrollbar width problem.
 			if( instance.options.liquid )
 				$('html').css( 'overflow-y', 'scroll' );
-			
+
 			// save the heights of all images.
 			if( !instance.options.fixedHeight ) {
 				instance.$imgs.each( function(i) {
 					var $img	= $(this), img_w = $img.width();
-					
+
 					// if images have width > instance.options.minw then "resize" image.
 					if( img_w < instance.options.minw && !instance.options.minsize ) {
 						var new_h = instance._getImageHeight( $img, instance.options.minw );
@@ -341,32 +341,32 @@
 					}
 					else {
 						instance.heights.push( $img.height() );
-					}	
+					}
 				});
 			}
-			
+
 			// calculate which height to use for each image.
 			instance.theHeight			= ( !instance.options.fixedHeight && !instance.options.alternateHeight ) ? instance._chooseHeight() : instance.options.fixedHeight;
-			
+
 			if( instance.options.alternateHeight )
 				instance.theHeight		= Math.floor( Math.random() * ( instance.options.alternateHeightRange.max - instance.options.alternateHeightRange.min + 1 ) + instance.options.alternateHeightRange.min );
-				
+
 			// save some values.
 			instance.cache.container_w	= el_w;
 			// space left to fill the row.
 			instance.cache.space_w_left = el_w;
-			
+
 			// wrap the images with the right sizes.
 			instance.$imgs.each( function(i) {
 				instance._insertImage( $(this) );
 			});
-			
+
 			if( instance.options.fillLastRow && instance.cache.space_w_left !== instance.cache.container_w ) {
 				instance._stretchImage( instance.$imgs.eq( instance.totalImages - 1 ) );
 			}
-			
+
 			// window resize event : reload the container.
-			$(window).bind('smartresize.montage', function() { 
+			$(window).bind('smartresize.montage', function() {
 				instance._reload();
 			});
 		},
@@ -382,10 +382,10 @@
 			$images.find('img').each( function(i) {
 				instance._insertImage( $(this) );
 			});
-			
+
 			if( instance.options.fillLastRow && instance.cache.space_w_left !== instance.cache.container_w )
 				instance._stretchImage( instance.$imgs.eq( instance.totalImages - 1 ) );
-			
+
 			if ( callback ) callback.call( $images );
 		},
 		destroy				: function( callback ) {
@@ -399,24 +399,24 @@
 				left		: '',
 				top			: ''
 			}).unwrap();
-			
+
 			if( this.options.liquid )
 				$('html').css( 'overflow', '' );
-			
+
 			this.element.unbind('.montage').removeData('montage');
 
 			$(window).unbind('.montage');
-			
+
 			if ( callback ) callback.call();
 		},
 		option				: function( key, value ) {
 			// set options AFTER initialization:
 			if ( $.isPlainObject( key ) ){
 				this.options = $.extend( true, this.options, key );
-			} 
+			}
 		}
 	};
-	
+
 	// taken from jquery.masonry
 	// 	 https://github.com/desandro/masonry
 	// helper function for logging errors
@@ -426,14 +426,14 @@
 			console.error( message );
 		}
 	};
-	
+
 	// Structure taken from jquery.masonry
 	// 	 https://github.com/desandro/masonry
 	// =======================  Plugin bridge  ===============================
 	// leverages data method to either create or return $.Montage constructor
 	// A bit from jQuery UI
 	//   https://github.com/jquery/jquery-ui/blob/master/ui/jquery.ui.widget.js
-	// A bit from jcarousel 
+	// A bit from jcarousel
 	//   https://github.com/jsor/jcarousel/blob/master/lib/jquery.jcarousel.js
 
 	$.fn.montage 				= function( options ) {
@@ -455,7 +455,7 @@
 				// apply method
 				instance[ options ].apply( instance, args );
 			});
-		} 
+		}
 		else {
 			this.each(function() {
 				var instance = $.data( this, 'montage' );
@@ -463,15 +463,15 @@
 					// apply options & reload
 					instance.option( options || {} );
 					instance._reload();
-				} 
+				}
 				else {
 					// initialize new instance
 					$.data( this, 'montage', new $.Montage( options, this ) );
 				}
 			});
 		}
-		
+
 		return this;
 	};
-	
+
 })( window, jQuery );
