269bbf0b9cd00a952ba621c39bb879eb6e81b919
Anderson Mills <andersonrmills@gmail.com>
2016-09-12 18:01:31 -0700

    added billboard newsfeed

--- js/news_feeds.js
+++ js/news_feeds.js
@@ -27,6 +27,16 @@ $(function() {
           });
         });
 
+        $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.billboard.com%2Frss%2Fcharts%2Fhot-100%3Fformat%3Dxml%22&format=json&diagnostics=true&callback=",
+          function(response) {
+            $.each(response.query.results.item, function (i,newsItem) {
+                var RF_description = newsItem.description.split("...<br/>")[0];
+                $("#Billboard-cont").append('<div class="media-news-item"><a class="pull-left" target="_blank" href="'+newsItem.link+'"><img class="media-object" src="img/billboard.jpg"  alt="100x100" style="width: 100px; height: 100px;"></a><div class="media-body"><h4 class="media-heading">' + newsItem.title + '</h4><p>' + RF_description + '... <a target="_blank" href="' + newsItem.link + '">more </a></p></div></div>');
+
+                return ( i !== (numArticles -1) );
+            });
+          });
+
         // $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.allaccess.com%2Ffeed%2Fnet-news.rss%22&format=json&diagnostics=true&callback=",
         //   function(response) {
         //     // console.log();
